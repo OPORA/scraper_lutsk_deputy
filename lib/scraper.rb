c05cda2bd915cbe44ce5c1213319af4863afda61
Beastie PC <gskondratenko@opora.org.ua>
2017-05-14 18:19:32 +0300

    Change scraper for lutsk deputy #1

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,27 +4,30 @@ require_relative './people'
 
 class ScrapeMp
   def parser
-    DataMapper.auto_upgrade!
-    url = "http://drv.vmr.gov.ua/deputaty.aspx"
-    page = get_page(url)
-    page.css('#form1 table tr td div  table tr td:nth-child(1)').each do |mp|
-
-      mp.css('table .ELEMENTY_LABEL').each_with_index do |fio, index|
-        faction = mp.css("table #ContentPlaceHolder1_ExampleRepeater_LAB_PARTIYA_#{index}").text
-        next if faction == "уповноважений"
-        okrug = mp.css("table #ContentPlaceHolder1_ExampleRepeater_LAB_OKRUG_#{index}").text
-        full_name = fio.text
-        photo_url = mp.css("table #ContentPlaceHolder1_ExampleRepeater_IMG_FOTO_#{index}")[0]["src"]
-        scrape_mp(full_name, okrug, faction, photo_url)
+    #DataMapper.auto_upgrade!
+    url_all = "http://www.lutskrada.gov.ua/deputy?field_person_district_tid=All&field_person_faction_tid=All"
+    page_number = get_page(url_all)
+    pages = page_number.css('.pager-last a')[0][:href][/page=.+/][/\d+/].to_i
+    i = 2000
+    (0..pages).each do |p|
+      url = "http://www.lutskrada.gov.ua/deputy?field_person_district_tid=All&field_person_faction_tid=All&page=#{p}"
+      page = get_page(url)
+      page.css('table.views-view-grid tr').each do |tr|
+        tr.css('td').each do |mp|
+         full_name = mp.css('.views-field-title a').text
+         faction = mp.css('.views-field-field-person-faction a').text
+         photo_url = mp.css('.views-field-field-person-photo img')[0][:src]
+         i = i + 1
+         scrape_mp(full_name, nil, faction, photo_url, i)
+        end
       end
-
     end
     #resigned_mp()
     create_mer()
   end
   def create_mer
     #TODO create mer Sadovoy
-    names = %w{Моргунов Сергій Анатолійович}
+    names = %w{Поліщук Ігор Ігорович}
     People.first_or_create(
         first_name: names[1],
         middle_name: names[2],
@@ -32,7 +35,7 @@ class ScrapeMp
         full_name: names.join(' '),
         deputy_id: 1111,
         okrug: nil,
-        photo_url: "http://www.vmr.gov.ua/SiteAssets/Lists/CityMajor/Default/Міський голова Сергій Моргунов.jpg",
+        photo_url: "http://www.lutskrada.gov.ua/sites/default/files/polishchuk_igor_igorovych_1.jpg",
         faction: nil,
         end_date: nil,
         created_at: "9999-12-31"
@@ -46,7 +49,7 @@ class ScrapeMp
     page_resigned = get_page(uri)
     scrape_mp( )
   end
-  def scrape_mp(fio, okrug, party, image, date_end=nil)
+  def scrape_mp(fio, okrug, party, image, rada_id, date_end=nil)
     party = case
               when party[/Солідарність/]
                 "Блок Петра Порошенка"
@@ -56,19 +59,24 @@ class ScrapeMp
                 "Батьківщина"
               when party[/Свобода/]
                 "Свобода"
+              when party[/УКРОП/]
+                "УКРОП"
+              when party[/Народний контроль/]
+                "Народний контроль"
+              when party[/Радикальної Партії/]
+                "Радикальна партія"
+              when (fio == "Козлюк Олександр Євгенович" or fio == "Ткачук Євгеній Євгенович" or fio == "Яручик Микола Олександрович")
+                "Блок Петра Порошенка"
               else
                 party
             end
-    rada_id = image[/\d+/]
     name = fio.gsub(/\s{2,}/,' ')
-    image = "http://drv.vmr.gov.ua/" + image.strip
     name_array = name.split(' ')
     people = People.first(
         first_name: name_array[1],
         middle_name: name_array[2],
         last_name: name_array[0],
         full_name: name_array.join(' '),
-        deputy_id: rada_id,
         okrug: okrug,
         photo_url: image,
         faction: party,
